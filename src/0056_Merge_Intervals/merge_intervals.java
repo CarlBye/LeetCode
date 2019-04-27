666cb3889f7d750b1a1b839a35631244b7502c18
CarlBye <carl.pilgriim@gmail.com>
2019-04-27 21:43:11 +0800

    add 56 java

---
+++ src/0056_Merge_Intervals/merge_intervals.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) return intervals;
+        Arrays.sort(intervals, new Comparator<int[]>(){
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+        List<Interval> list = new ArrayList<>();
+        list.add(new Interval(intervals[0][0], intervals[0][1]));
+        Interval last;
+        for (int i = 0; i < intervals.length; i++) {
+            last = list.get(list.size() - 1);
+            if (last.end < intervals[i][0]) {
+                list.add(new Interval(intervals[i][0], intervals[i][1]));
+            } else {
+                last.end = Math.max(intervals[i][1], last.end);
+            }
+        }
+        int size = list.size();
+        int[][] ans = new int[size][2];
+        for (int i = 0; i < size; i++){
+            ans[i][0] = list.get(i).start;
+            ans[i][1] = list.get(i).end;
+        }
+        return ans;
+    }
+}
